fcd776ad7761587799047e5333a271db6e209b22
Kevin Chi <kevinchi118@gmail.com>
2018-06-16 22:07:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
     def name=(dogs_name)
-        @this_dogsname = name
+        @this_dogs_name = dogs_name
     end
 
     def name
-        @name
+        @this_dogs_name = dogs_name
     end
 end
 
